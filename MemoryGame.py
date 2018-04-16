9531579ef2b118399aac7b8747bfcff3e748123c
Beenish-Syed <38268487+Beenish-Syed@users.noreply.github.com>
2018-04-16 12:01:07 -0700

    Update MemoryGame.py

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,4 +1,3 @@
-#Check
 import random
 import java.awt.Font as Font
 import os
@@ -9,45 +8,27 @@ class Coords:
   def __init__(self,i):
     self.x = i%cols
     self.y = i//cols
-
-#Takes the list of coordinates [x, y] as an input
-def clearCardAtCoord(c, image):
-  startX = (c[0] + 1)*margins  + (c[0] * cardWidth)
-  startY = (c[1] + 1)*margins  + (c[1] * cardHeight)
-  for i in range(cardWidth):
-    for j in range(cardHeight):
-      pixel = getPixel(image, cardWidth + i, cardHeight + j)
-      setColor(pixel, backgroundColor)
-      return image
-
-def hideCardAtCoord(c, image, back):
-  startX = (c[0] + 1)*margins  + (c[0] * cardWidth)
-  startY = (c[1] + 1)*margins  + (c[1] * cardHeight)
-  for i in range(cardWidth):
-    for j in range(cardHeight):  
-      pixel = getPixel(image, startX + i, startY + j)
-#*****This code will be implemented once I create an image for back of card
-#     backPixel = getPixel(cardBack, i, j)
-#     color = getcolor
-#     setColor(pixel, color)
-      setColor(pixel, cardBackColor)   
-      return image
-  #Draws/shows card at the given location
+    self.up = 0 # Initially card facedown 0 for facedown, 1 for face up, -1 for clear
   
 #Adds/draws image at the given location
 def showCardAtCoord(cd, boardImage):
   x = cd.x
   y = cd.y
-  image = images[board[y][x]] # gets one image from the cards/images list
+  if cd.up == 0:
+    image = backImages[backboard[y][x]]
+  elif cd.up == -1:
+    image = clearImage
+  elif cd.up ==1:
+    image = images[board[y][x]] # gets one image from the cards/images list
   width = getWidth(image) #gets width of a single card
-  height = getHeight(image)#gets height of a single card
+  height = getHeight(image)#gets height of a single care
   for m in range(width):
     for n in range (height):
       pixel = getPixel(image, m,n)
       color = getColor(pixel) 
       newPixel = getPixel(boardImage, (width*x)+(margins*(x+1))+m , (height*y)+(margins*(y+1))+n ) 
       setColor(newPixel, color)
-  show(boardImage)
+  repaint (boardImage)
   return boardImage
 
   
@@ -75,6 +56,22 @@ def playSound(num): #num corresponds to the sound we want to play. It is the key
   soundDict = {1:win, 2:lose, 3:match, 4:wrong}
   play(soundDict[num])
   
+def initializeBoardImage(boardImage):
+  for i in range(12):
+    cd = intToCoord(i)
+    x = cd.x
+    y = cd.y
+    cd.up = 0
+    boardImage = showCardAtCoord(cd, boardImage)
+  return boardImage
+    
+def intToCoord(i):
+  coord = Coords(i)
+  return coord
+    
+def getInput():
+  x = int(requestString("Enter the number of image you want to turn up. 0-11"))
+  return x
 boardWidth = 800
 boardHeight = 600
 cols = 4
@@ -82,11 +79,10 @@ rows = 3
 margins = 5
 cardWidth = (boardWidth-margins*(cols+1))/cols
 cardHeight = (boardHeight-margins*(rows+1))/rows
-cardBackColor = makeColor(127,127,127)
-#cardBack = makePicture("C:\******\cardReverse.jpg") once a make a card back image
-#cardBack = scalePercent
-backgroundColor = makeColor(31,31,31)
-boardImage = makeEmptyPicture(800,600)
+boardImage = makeEmptyPicture(800,600,blue)
+backboard =[[0,1,2,3],
+           [4,5,6,7],
+           [8,9,10,11]]
 board =  [[0,0,1,1],
           [2,2,3,3],
           [4,4,5,5]]
@@ -97,8 +93,9 @@ maxInt = len(imagePaths)-1
 validIndexes = []
 print("Resizing images in folder.")
 
+images = []
 for i in range(0,6): #Resize 6 base images from the folder, and append the resized version to images[]
-  ri = random.randint(0,maxInt)
+  ri = i# = random.randint(0,maxInt)
   resized = makePicture(imageFolder+"\\"+imagePaths[ri])
   imagePaths[ri] = imagePaths[maxInt]
   ratio = cardWidth/float(getWidth(resized)) #Percent required to match width
@@ -108,23 +105,49 @@ for i in range(0,6): #Resize 6 base images from the folder, and append the resiz
   resized = scalePercent(resized,ratio*100)
   images.append(resized)
   maxInt -= 1
-randomizeBoard()
-hideAllCards()
-show(boardImage)
-print board
+
+backImageFolder = os.path.dirname(os.path.abspath(__file__))+r"\MemoryGameBackImages"
+backImagesPath= os.listdir(backImageFolder)
+backImages= []
+for i in range(len(backImagesPath)):
+  backImage = makePicture(backImageFolder+"\\"+backImagesPath[i])
+  ratio = cardWidth/float(getWidth(backImage)) #Percent required to match width
+  if ratio > cardHeight/float(getHeight(backImage)):
+    ratio = cardHeight/float(getHeight(backImage)) #Percent required to match height
+  #Rescale by the smaller ratio, to ensure the new image fits into the card area, even if it isn't square
+  backImages.append(scalePercent(backImage,ratio*100))
+ 
+clearImage = makePicture(path+"\\clearImage.png")
+ratio = cardWidth/float(getWidth(clearImage)) #Percent required to match width
+if ratio > cardHeight/float(getHeight(clearImage)):
+  ratio = cardHeight/float(getHeight(clearImage)) #Percent required to match height
+  #Rescale by the smaller ratio, to ensure the new image fits into the card area, even if it isn't square
+clearImage = scalePercent(clearImage,ratio*100)
+ 
+boardImage = initializeBoardImage(boardImage)
 wrongTurns = 0
 matchesMade = 0
 while true:
-  guessOne = -1 #Reset guessOne, this variable is used in the getInput function, so you can't pick the same card twice.
+  #guessOne = -1 #Reset guessOne, this variable is used in the getInput function, so you can't pick the same card twice.
   i1 = getInput()
   c1 = intToCoord(i1)
-  showCardAtCoord(c1)
+  while (board[c1.y][c1.x]== -1):
+    print "You have already matched that card. Please pick another card"
+    i1 = getInput()
+    c1 = intToCoord(i1)    
+  c1.up =1 #Show card faceup
+  print i1, c1.x, c1.y
+  boardImage = showCardAtCoord(c1, boardImage)
   repaint(boardImage)
-  guessOne = i1
 
   i2 = getInput()
   c2 = intToCoord(i2)
-  showCardAtCoord(c2)
+  while (board[c2.y][c2.x]== -1) or (i1 == i2):
+    print "You have already matched/picked that card. Please pick another card"
+    i2 = getInput()  
+    c2 = intToCoord(i2)  
+  c2.up =1 #show card faceup
+  boardImage=showCardAtCoord(c2,boardImage)
   repaint(boardImage)
 
   if board[c1.y][c1.x] == board[c2.y][c2.x]:
@@ -132,19 +155,24 @@ while true:
     board[c2.y][c2.x] = -1
     playSound(3) #plays the sound for key 3 in soundDict
     showInformation("Match!")
-    boardImage = clearCardAtCoord(c1, boardImage) #I added a boardImage Parameter. Not sure how this would be done without. Maybe I am missing something? - WB
-    boardImage = clearCardAtCoord(c2, boardImage)
+    c1.up = -1
+    c2.up = -1
+    boardImage = showCardAtCoord(c1, boardImage)
+    boardImage = showCardAtCoord(c2, boardImage)
     matchesMade +=1
     if matchesMade >=6:
-      playSound(1) #plays the sound for key 1 in soundDict
+      playSound(1)
       showInformation("You win!")
+       #plays the sound for key 1 in soundDict
       break
   else:
     wrongTurns += 1
     playSound(4) #plays sound for key 4 in soundDict
+    c1.up = 0
+    boardImage = showCardAtCoord(c1, boardImage)
+    c2.up = 0
+    boardImage = showCardAtCoord(c2, boardImage)
     showInformation("No match! %d/6 wrong moves"%wrongTurns)
-    boardImage = hideCardAtCoord(c1, boardImage)
-    boardImage = hideCardAtCoord(c2, boardImage)
     if wrongTurns >= 6:
       playSound(2) #plays sound for key 2
       showInformation("You lose!")
